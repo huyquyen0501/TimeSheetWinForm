1d930872bd10887371e3ac6f06f8f02393440485
huy.tranduc.ncc <huy11a2@live.com>
2020-03-18 19:57:03 +0700

    Base

--- Form1.cs
+++ Form1.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using TimeSheetWinForm.Entites;
+using TimeSheetWinForm.Utils;
 namespace TimeSheetWinForm
 {
     public partial class Form1 : Form
@@ -15,6 +16,74 @@ namespace TimeSheetWinForm
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        #region
+        static TimeSheetModel TimeSheetModel = new TimeSheetModel();
+        
+        public bool CheckPassWord(string PassWordUnHash, string Hashed)
+        {
+            return BCrypt.Net.BCrypt.Verify(PassWordUnHash, Hashed);
+        }
+
+        #endregion
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var UserName = textBox1.Text;
+            var password = textBox2.Text;
+            if(UserName ==null || password == null)
+            {
+                MessageBox.Show("Nhap vao username va mat khau", "Loi");
+            }
+            var a = TimeSheetModel.User.Where(s => s.UserName == UserName).Select(s => new
+            {
+                UserName = s.UserName,
+                //Role = s.Roles.RoleName,
+                UserId = s.Id,
+                password=s.PassWord,
+                Name=s.Name
+            }).ToList();
+            if(a.Any())
+            {
+               
+                if (CheckPassWord(password, a.First().password))
+                {
+                    Session.UserSessionId = a.First().UserId;
+                    Session.Name = a.First().UserName;
+                    Session.RoleNameOfUser  = TimeSheetModel.UserRole.Where(s => s.UserId == Session.UserSessionId).Select(s => s.Roles.RoleName).ToList();
+                    Form2 form2 = new Form2();
+                    form2.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Sai ten dang nhap hoac mat khau", "Error");
+                }
+            }
+            else { MessageBox.Show("Sai ten dang nhap hoac mat khau", "Error"); }
+
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Register register = new Register();
+            register.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            textBox2.PasswordChar = '@';
         }
     }
 }
---
+++ Form2.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TimeSheetWinForm.Utils;
+
+namespace TimeSheetWinForm
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            this.IsMdiContainer = true;
+            InitializeComponent();
+        }
+        #region
+        private static bool _exiting;
+        static TimeSheetModel TimeSheetModel = new TimeSheetModel();
+        #endregion
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            label1.Text = $"Xin chào {Session.Name}";
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+            button7.Enabled = true;
+            button8.Enabled = false;
+            foreach(var a in Session.RoleNameOfUser)
+            {
+                switch (a)
+                {
+                    case "Admin":
+                        button1.Enabled = button2.Enabled = button3.Enabled = button4.Enabled = button5.Enabled = button6.Enabled = button7.Enabled = button8.Enabled = true;
+                        break;
+                    case "ProjectAdmin":
+                         button2.Enabled = button3.Enabled = button4.Enabled = button5.Enabled = button6.Enabled = button7.Enabled = button8.Enabled = true;
+                        break;
+                    case "SuperVisor":
+                        button2.Enabled = button3.Enabled = button5.Enabled = button8.Enabled = true;
+                        break;
+                    case "BasicUser":
+                        button6.Enabled = true;
+                        break;
+                        
+
+                }
+
+            }
+            //Neu Basicuser nhung duoc lam PM cua du an thi se bat duoc cac nut manage project,managetask, timesheet nhung chi duoc phep xem du an no lam quan ly
+            //(xu ly viec hien ra o  cac code cua doan sau nhe, o day chi xu ly viec bat nut)
+            if (TimeSheetModel.ProjectUsers.Any(s => s.UserId == Session.UserSessionId && s.Type == Entites.ProjectUserType.PM))
+            {
+                button2.Enabled = button4.Enabled = button5.Enabled = true;
+            }
+            
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            
+            if (!_exiting && MessageBox.Show("Bạn có chắc chắn muốn thoát?","Messenger",
+                       MessageBoxButtons.OKCancel,
+                       MessageBoxIcon.Information) == DialogResult.OK)
+            {
+                _exiting = true;
+                Session.UserSessionId = 0;
+                Session.Name = "";
+                Session.RoleNameOfUser = null;
+                Environment.Exit(1);
+            }
+           
+            
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ManagerUser managerUser = new ManagerUser();
+            managerUser.MdiParent = this;
+            managerUser.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ManagerProject managerProject = new ManagerProject();
+            managerProject.MdiParent = this;
+            managerProject.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ManageClient manageClient = new ManageClient();
+            manageClient.MdiParent = this;
+            manageClient.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ManageTask manageTask = new ManageTask();
+            manageTask.MdiParent = this;
+            manageTask.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ManagerTimesheet managerTimesheet = new ManagerTimesheet();
+            managerTimesheet.MdiParent = this;
+            managerTimesheet.Show();
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ManageMyTimesheet manageMyTimesheet = new ManageMyTimesheet();
+            manageMyTimesheet.MdiParent = this;
+            manageMyTimesheet.Show();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Config config = new Config();
+            config.MdiParent = this;
+            config.Show();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            ManageTimesheetStatic manageTimesheetStatic = new ManageTimesheetStatic();
+            manageTimesheetStatic.MdiParent = this;
+            manageTimesheetStatic.Show();
+        }
+    }
+}
