2321b7df0d33b37c18ade9db1f9360876c2f2d94
huy.tranduc.ncc <huy11a2@live.com>
2020-04-02 00:25:08 +0700

    update detail project user

--- ManageProject/DetailProject/DetailProjectUser.Designer.cs
+++ ManageProject/DetailProject/DetailProjectUser.Designer.cs
@@ -33,6 +33,10 @@
             this.label2 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -49,42 +53,86 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(203, 324);
+            this.label1.Location = new System.Drawing.Point(208, 284);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.Size = new System.Drawing.Size(76, 17);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "TaskName";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(528, 324);
+            this.label2.Location = new System.Drawing.Point(532, 284);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.Size = new System.Drawing.Size(48, 17);
             this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.label2.Text = "Status";
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(293, 324);
+            this.comboBox1.Location = new System.Drawing.Point(293, 284);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 24);
             this.comboBox1.TabIndex = 3;
+            this.comboBox1.Click += new System.EventHandler(this.comboBox1_Click);
             // 
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(622, 321);
+            this.comboBox2.Location = new System.Drawing.Point(611, 281);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(121, 24);
             this.comboBox2.TabIndex = 4;
+            this.comboBox2.Click += new System.EventHandler(this.comboBox2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(62, 399);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Thêm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(234, 399);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Sửa";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(397, 399);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Xóa";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(611, 388);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(121, 34);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Xác nhận";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // DetailProjectUser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label2);
@@ -106,5 +154,9 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
--- ManageProject/DetailProject/DetailProjectUser.cs
+++ ManageProject/DetailProject/DetailProjectUser.cs
@@ -26,8 +26,12 @@ namespace TimeSheetWinForm.ManageProject.DetailProject
         List<string> Combobox2Source = new List<string> { "PM", "Member" };
         void LoadData()
         {
-            dataGridView1.DataSource = ListDataGridViewSource;
-            
+            dataGridView1.DataSource = ListDataGridViewSource; 
+        }
+        void loadcombobox()
+        {
+            comboBox1.Text= dataGridView1.Rows[0].Cells[1].Value.ToString();
+            comboBox2.Text= dataGridView1.Rows[0].Cells[2].Value.ToString();
         }
         #endregion
         private void DetailProjectUser_Load(object sender, EventArgs e)
@@ -44,10 +48,98 @@ namespace TimeSheetWinForm.ManageProject.DetailProject
                 ListDataGridViewSource.Add(a);
             }
             LoadData();
-            var listcombobox1 = TimeSheetModel.User
+            var a1 = ListDataGridViewSource.Select(t => t.UserId).ToList();
+            var listcombobox1 = TimeSheetModel.User.Where(s=>a1.Contains(s.Id)==false).ToList();
+            foreach(var a in listcombobox1)
+            {
+                User user = new User();
+                user.UserName = a.UserName;
+                user.Id = a.Id;
+                ListCombobox1Source.Add(user);
+            }
             if (ListDataGridViewSource.Count() > 0)
             {
-                comboBox1.Text=
+                comboBox1.Text = dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].Cells[1].Value.ToString();
+            }
+            BindingSource bs = new BindingSource();
+            bs.DataSource = Combobox2Source;
+            comboBox2.DataSource = bs;
+        }
+
+        private void comboBox1_Click(object sender, EventArgs e)
+        {
+            comboBox1.DataSource = ListCombobox1Source;
+            comboBox1.DisplayMember = "UserName";
+            comboBox1.Invalidate();
+            comboBox1.Text= dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].Cells[1].Value.ToString();
+        }
+
+        private void comboBox2_Click(object sender, EventArgs e)
+        {
+            comboBox2.Text = dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].Cells[2].Value.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!ListDataGridViewSource.Any(s => s.UserName == comboBox1.Text))
+                {
+                    var pu = new ProjectUserDto
+                    {
+                        ProjectId = ManagerProject.ProjectId,
+                        Type = comboBox2.Text,
+                        UserName = comboBox1.Text,
+                        UserId = ListCombobox1Source.Where(s => s.UserName == comboBox1.Text).Select(s => s.Id).FirstOrDefault()
+                    };
+                    ListDataGridViewSource.Add(pu);
+                    foreach (var a in ListCombobox1Source.ToList())
+                    {
+                        if (a.Id == pu.UserId)
+                        {
+                            ListCombobox1Source.Remove(a);
+                            break;
+                        }
+                    }
+                    LoadData();
+                    loadcombobox();
+                }
+                else { MessageBox.Show($"Nhân viên {comboBox1.Text} đã có trong dự án"); }
+            }catch(Exception ex)
+            {
+                MessageBox.Show($"{ex}");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!TimeSheetModel.MyTimesheets.Any(s => s.UserId == long.Parse(dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].Cells[0].Value.ToString()) && s.ProjectTask.ProjectId == ManagerProject.ProjectId)) {
+
+                    var tempedObject = ListDataGridViewSource.Where(s => s.UserId == long.Parse(dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].Cells[0].Value.ToString())).Select(s => s).FirstOrDefault();
+                    foreach (var a in ListDataGridViewSource.ToList())
+                    {
+                        if (a == tempedObject) { ListDataGridViewSource.Remove(a); }
+                    }
+
+                    User task = new User
+                    {
+                        Id = tempedObject.UserId,
+                        Name = tempedObject.UserName
+                    };
+                    ListCombobox1Source.Add(task);
+                    LoadData();
+                    loadcombobox();
+                }
+                else
+                {
+                    MessageBox.Show($"User {comboBox1.Text} had logged timesheet, can't delete");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"không thể xóa dòng không có thông tin");
             }
         }
     }
