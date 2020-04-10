7850c27da318ab7d124d2c52960c23b767f06bdd
nguyenducthong <55329441+nguyenducthong@users.noreply.github.com>
2020-04-10 22:25:04 +0700

    lam giao dien

--- ManageTask.cs
+++ ManageTask.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TimeSheetWinForm.Entites;
 
 namespace TimeSheetWinForm
 {
@@ -15,6 +16,57 @@ namespace TimeSheetWinForm
         public ManageTask()
         {
             InitializeComponent();
+        }
+        #region
+        TimeSheetModel Timesheetmodel = new TimeSheetModel();
+
+        #endregion
+        private void ManageTask_Load(object sender, EventArgs e)
+        {
+            dgvtask.DataSource = Timesheetmodel.Tasks.Where(s => s.IsDeleted != true).Select(s => new
+            {
+                ID = s.Id,
+                Name = s.Name,
+                TypeTask = (StatusEnum.TaskType)s.Type
+            }).ToList();
+
+            loadcombobox();
+        }
+
+        void loadcombobox()
+        {
+            //StatusEnum.TaskType d = new StatusEnum.TaskType();
+            cbxtype.DataSource = Enum.GetValues(typeof(StatusEnum.TaskType));
+
+            //int d = int.Parse(cbxtype.SelectedValue.ToString());
+            //cbxtype.DisplayMember = d;
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+           
+            Entites.Task task = new Entites.Task();
+            var d = cbxtype.SelectedItem.ToString();
+            var s = cbxtype.SelectedIndex;
+            //string s = "StatusEnum.TaskType." + d;
+
+            //StatusEnum.TaskType b = (int)s;
+            Array allenum = Enum.GetValues(typeof(StatusEnum.TaskType));//lấy ra tất cả các phần tử enum
+           
+            try
+            {
+                if(txtname.Text != null)
+                {
+                    task.Name = txtname.Text;
+                    //task.Type = 
+                }
+            }
+            catch
+            {
+
+            }
+            
+
         }
     }
 }
